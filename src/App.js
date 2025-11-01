62b0433eab4c7541f3956bf37acd0b471c30dad3
anit7215 <tina112703@gmail.com>
2025-11-02 00:19:13 +0900

    feat(App): 구입금액 입력 및 로또 개수 변환 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Console } from "@woowacourse/mission-utils";
+
 class App {
-  async run() {}
+  async run() {
+    const purchaseAmount = await Console.readLineAsync(
+      "구입금액을 입력해 주세요.\n"
+    );
+    Console.print(this.amountToLottoCount(purchaseAmount));
+  }
+
+  amountToLottoCount(purchaseAmount) {
+    if (purchaseAmount > 0 && purchaseAmount % 1000 !== 0)
+      return "[ERROR] 구입금액은 1,000원 단위로 입력해 주세요.";
+    return purchaseAmount / 1000;
+  }
 }
 
 export default App;
