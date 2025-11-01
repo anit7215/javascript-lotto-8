9a4b52f5191b6af6bbaefa5425c69e8078fd8ec7
anit7215 <tina112703@gmail.com>
2025-11-02 01:23:18 +0900

    feat(App): 로또 개수 & 로또 번호 출력 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
-import { Console } from "@woowacourse/mission-utils";
+import { Console, Random } from "@woowacourse/mission-utils";
 
 class App {
+  LottoNumbers = [];
+
   async run() {
     const purchaseAmount = await Console.readLineAsync(
       "구입금액을 입력해 주세요.\n"
     );
-    Console.print(this.amountToLottoCount(purchaseAmount));
+    const lottoAmount = this.amountToLottoCount(purchaseAmount);
+    this.printLottoNumber(lottoAmount);
   }
 
   amountToLottoCount(purchaseAmount) {
-    if (purchaseAmount > 0 && purchaseAmount % 1000 !== 0)
-      return "[ERROR] 구입금액은 1,000원 단위로 입력해 주세요.";
+    if (purchaseAmount > 0 && purchaseAmount % 1000 !== 0) {
+      throw new Error("[ERROR] 구입금액은 1,000원 단위로 입력해 주세요.");
+    }
     return purchaseAmount / 1000;
+  }
+
+  printLottoNumber(lottoCount) {
+    Console.print(`\n${lottoCount}개를 구매했습니다.`);
+    let count = lottoCount;
+    while (count > 0) {
+      const lottoNumber = this.generateLottoNumber();
+      Console.print(`[${lottoNumber.join(", ")}]`);
+      this.LottoNumbers.push(lottoNumber);
+      count--;
+    }
+  }
+
+  generateLottoNumber() {
+    return Random.pickUniqueNumbersInRange(1, 45, 6).sort((a, b) => a - b);
   }
 }
 
