585742d7e7bedfa49f1ec06adf1e82197dbd1d5e
anit7215 <tina112703@gmail.com>
2025-11-03 20:53:37 +0900

    fix(Validator): 보너스 번호 검증 로직 수정

--- src/utils/Validator.js
+++ src/utils/Validator.js
@@ -29,13 +29,14 @@ class Validator {
   }
 
   static validateBonusNumber(bonusNumber, winningNumbers) {
-    if (isNaN(bonusNumber)) {
+    const number = Number(bonusNumber);
+    if (isNaN(number)) {
       throw new Error("[ERROR] 숫자만 입력해 주세요.");
     }
-    if (bonusNumber < 1 || bonusNumber > 45) {
+    if (number < 1 || number > 45) {
       throw new Error("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
     }
-    if (winningNumbers.includes(bonusNumber)) {
+    if (winningNumbers.includes(number)) {
       throw new Error("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
     }
   }
