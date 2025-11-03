61d3b205e8bce0e14d3a43a7ffbb62b5c4d2efa3
anit7215 <tina112703@gmail.com>
2025-11-03 20:46:01 +0900

    test(LottoTest): 로또 번호 유효성 및 당첨 계산 테스트 코드 추가

--- __tests__/LottoTest.js
+++ __tests__/LottoTest.js
@@ -15,4 +15,28 @@ describe("로또 클래스 테스트", () => {
   });
 
   // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+  test("번호가 1~45를 벗어나면 예외가 발생한다.", () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 46]);
+    }).toThrow("[ERROR]");
+  });
+});
+
+describe("로또 당첨 결과 계산 테스트", () => {
+  const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+  const winningNumbers = [1, 2, 3, 7, 8, 9];
+
+  test("당첨 번호와 일치하는 개수를 정확히 계산한다.", () => {
+    expect(lotto.countMatches(winningNumbers)).toBe(3);
+  });
+
+  test.each([
+    [5, true], // 보너스 번호 포함
+    [10, false], // 보너스 번호 미포함
+  ])(
+    "보너스 번호가 포함되어 있는지를 판단한다.",
+    (bonusNumber, expected) => {
+      expect(lotto.hasBonus(bonusNumber)).toBe(expected);
+    }
+  );
 });
